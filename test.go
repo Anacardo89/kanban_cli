01b7846bbbce5e4c23aba93bd055bdbeea4a8e80
anacardo <pedro.fortunato.89@gmail.com>
2024-01-15 15:15:58 +0000

    Adjusts cursor for infinite scrolling

--- test.go
+++ test.go
@@ -1,61 +1,61 @@
 package main
 
-import (
-	"log"
+// import (
+// 	"log"
 
-	"github.com/Anacardo89/kanban_cli/kanban"
-	"github.com/charmbracelet/lipgloss"
-)
+// 	"github.com/Anacardo89/kanban_cli/kanban"
+// 	"github.com/charmbracelet/lipgloss"
+// )
 
-func TestData() *kanban.Menu {
-	menu := kanban.StartMenu()
-	menu.AddProject("project1")
-	project1, err := menu.Projects.WalkTo(0)
-	if err != nil {
-		log.Println(err)
-	}
-	project1Val := project1.GetVal().(*kanban.Project)
-	project1Val.AddList("List 1")
-	menu.AddProject("project2")
-	project2, err := menu.Projects.WalkTo(1)
-	if err != nil {
-		log.Println(err)
-	}
-	project2Val := project2.GetVal().(*kanban.Project)
-	project2Val.AddList("List 1")
-	project1Val.AddList("List 2")
-	project2Val.AddList("List 2")
-	project1Val.AddLabel("Label 1", lipgloss.Color("30"))
-	project1Val.AddLabel("Label 2", lipgloss.Color("50"))
-	project2Val.AddLabel("Label 1", lipgloss.Color("30"))
-	project2Val.AddLabel("Label 2", lipgloss.Color("50"))
-	project1list1, err := project1Val.Lists.WalkTo(0)
-	if err != nil {
-		log.Println(err)
-	}
-	project1list2, err := project1Val.Lists.WalkTo(1)
-	if err != nil {
-		log.Println(err)
-	}
-	project2list1, err := project2Val.Lists.WalkTo(0)
-	if err != nil {
-		log.Println(err)
-	}
-	project2list2, err := project2Val.Lists.WalkTo(1)
-	if err != nil {
-		log.Println(err)
-	}
-	project1list1Val := project1list1.GetVal().(*kanban.List)
-	project1list2Val := project1list2.GetVal().(*kanban.List)
-	project2list1Val := project2list1.GetVal().(*kanban.List)
-	project2list2Val := project2list2.GetVal().(*kanban.List)
-	project1list1Val.AddCard("Card 1")
-	project1list1Val.AddCard("Card 2")
-	project1list2Val.AddCard("Card 1")
-	project1list2Val.AddCard("Card 2")
-	project2list1Val.AddCard("Card 1")
-	project2list1Val.AddCard("Card 2")
-	project2list2Val.AddCard("Card 1")
-	project2list2Val.AddCard("Card 2")
-	return menu
-}
+// func TestData() *kanban.Menu {
+// 	menu := kanban.StartMenu()
+// 	menu.AddProject("project1")
+// 	project1, err := menu.Projects.WalkTo(0)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	project1Val := project1.GetVal().(*kanban.Project)
+// 	project1Val.AddBoard("Board 1")
+// 	menu.AddProject("project2")
+// 	project2, err := menu.Projects.WalkTo(1)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	project2Val := project2.GetVal().(*kanban.Project)
+// 	project2Val.AddBoard("Board 1")
+// 	project1Val.AddBoard("Board 2")
+// 	project2Val.AddBoard("Board 2")
+// 	project1Val.AddLabel("Label 1", lipgloss.Color("30"))
+// 	project1Val.AddLabel("Label 2", lipgloss.Color("50"))
+// 	project2Val.AddLabel("Label 1", lipgloss.Color("30"))
+// 	project2Val.AddLabel("Label 2", lipgloss.Color("50"))
+// 	project1Board1, err := project1Val.Boards.WalkTo(0)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	project1Board2, err := project1Val.Boards.WalkTo(1)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	project2Board1, err := project2Val.Boards.WalkTo(0)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	project2Board2, err := project2Val.Boards.WalkTo(1)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	project1Board1Val := project1Board1.GetVal().(*kanban.Board)
+// 	project1Board2Val := project1Board2.GetVal().(*kanban.Board)
+// 	project2Board1Val := project2Board1.GetVal().(*kanban.Board)
+// 	project2Board2Val := project2Board2.GetVal().(*kanban.Board)
+// 	project1Board1Val.AddCard("Card 1")
+// 	project1Board1Val.AddCard("Card 2")
+// 	project1Board2Val.AddCard("Card 1")
+// 	project1Board2Val.AddCard("Card 2")
+// 	project2Board1Val.AddCard("Card 1")
+// 	project2Board1Val.AddCard("Card 2")
+// 	project2Board2Val.AddCard("Card 1")
+// 	project2Board2Val.AddCard("Card 2")
+// 	return menu
+// }
